b5bfda30f435dd9163008f58ee199a205a3e67a9
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-18 17:02:35 -0700

    added an addExercise function

--- Programming 2A/Unit 7/Final/fitnessTrackingApplication.py
+++ Programming 2A/Unit 7/Final/fitnessTrackingApplication.py
@@ -1,26 +1,78 @@
-'''
-needs exercises, reps, sets, duration, distance, laps, weight
-Strength training: Workout > exercises > sets > reps > weight
-Running: Distance > duration > laps >
+import os.path
 
-Exercises needs to be a list
-After choosing an exercise from the list you can add in your sets, reps, and the weight
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-needs to save in a txt file -- or somehow a sheet -- will figure out -- https://www.youtube.com/watch?v=X-L1NKoEi10 (Use Python To Import Data to Google Sheets)
+valueData = []
 
-'''
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-class strengthTraining():
-    def __init__(self, exercises):
-        self._exercises = exercises
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1Z-znKvKN-_29Jq7SCmNmXBczbgKCdMbHZq_vWEJJ0LA"
+SAMPLE_RANGE_NAME = "Sheet1!A2"
 
-    def listExercises(self):
-        print(f"Youre exercises are: {self._exercises}")
-        
-list = []
-for i in range(5):
-    exercise = input("Add an exercise to the list: ")
-    list.append(exercise)
+def addExercise():
+  # need to add an exercise name, the sets, and then reps.
+  exercise = []
+  exercise.append(input("What exercise did you do: "))
+  exercise.append(input("How many sets did you do: "))
+  exercise.append(input("How many reps did you do: "))
+  exercise.append(input("How much weight did you have: "))
+  exercise.append(input("What day was this? (xx/xx/xx): "))
 
-workout1 = strengthTraining(list)
-workout1.listExercises()
+  valueData.append(exercise)
+
+def main():
+  """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          r"C:\Users\reade\OneDrive\Documents\GitHub\FusionPythonCoding\Programming 2A\Unit 7\Final\credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=3000)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = (
+        sheet.values()
+        .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,valueInputOption="USER_ENTERED",body={"values": valueData})
+        .execute()
+    )
+    values = result.get("values", [])
+
+    if not values:
+      print("No data found.")
+      return
+
+    print("Name, Major:")
+    for row in values:
+      # Print columns A and E, which correspond to indices 0 and 4.
+      print(f"{row[0]}, {row[4]}")
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  addExercise()
+  main()
