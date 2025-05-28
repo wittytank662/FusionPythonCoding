4674e08406020dac2dcdf61c670cac2b229cbec8
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-28 13:42:34 -0700

    made progress on fitness tracking application

--- Programming 2A/Unit 7/Final/fitnessTrackingApplication.py
+++ Programming 2A/Unit 7/Final/fitnessTrackingApplication.py
@@ -33,7 +33,7 @@ class Workout():
     
   def saveData(self):
     # Saves the data in a .txt file
-    pass
+    return f"The workout on {self.day} is: {self.exercises}."
     
   def __str__(self):
     return f"The workout on {self.day} is: {self.exercises}."
@@ -51,6 +51,9 @@ class Exercise():
     (self.sets).append(whichSet)
     return f"Your sets are now: {self.sets}"
   
+  def saveData(self):
+    return f"Exercise: {self.exerciseName}, Sets: {self.sets}."
+  
   def __str__(self):
     return f"Exercise: {self.exerciseName}, Sets: {self.sets}."
   
@@ -60,7 +63,7 @@ class Exercise():
     
     
 class Set():
-  def __init__(self, reps, weight):
+  def __init__(self, reps = 0, weight = 0):
     self.reps = reps
     self.weight = weight
     
@@ -68,16 +71,43 @@ class Set():
     self.reps = reps
     return f"This set now has {self.reps} reps."
       
-  
+  def stringToObject(self, string):
+    reps = ""
+    weight = ""
+    for i in range(len(string)):
+      if string[i] == "R" and string[i + 1] == "e" and string[i + 2] == "p" and string[i + 3] == "s":
+        while string[i + 6] != ",":
+          reps = reps+string[i + 6]
+        print(reps)
+
   def setWeight(self, weight):
     self.weight = weight
     return f"The new weight is now: {self.weight}"
+  
+  def saveData(self):
+    return f"Reps: {self.reps}, Weight: {self.weight}"
   
   def __str__(self):
     return f"Reps: {self.reps}, Weight: {self.weight}"
   
   def __repr__(self):
     return self.__str__()
+    
+def saveData(workout):
+  file = open("data.txt", "a")
+  
+  file.write(workout.saveData())
+  file.write("\n")
+  file.close
+  
+def loadData():
+  pass
+
+def loadWorkout():
+  file = open("/Users/april/Developer/FusionPythonCoding/Programming 2A/Unit 7/Final/data.txt", "r")
+  
+  exerciseString = file.readline()
+  print(exerciseString)
     
 squatSet1 = Set(5, 80)
 squatSet2 = Set(5, 80)
@@ -93,4 +123,11 @@ pushups = Exercise("Pushups", [pushupSet1, pushupSet2, pushupSet3])
 
 workout1 = Workout([pushups, squats], "01/10/10")
 
-print(workout1)
+# print(workout1)
+
+# saveData(workout1)
+
+# loadWorkout()
+
+setDebug = Set()
+setDebug.stringToObject("Reps: 5, Weight: 0")
